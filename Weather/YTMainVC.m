39d9cf8cae10f27cec36c1f63a22a7be07be59a2
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2016-03-27 00:48:40 +0200

    showing current weather by geocode on the main view

--- Weather/YTMainVC.m
+++ Weather/YTMainVC.m
@@ -15,6 +15,14 @@
 
 @interface YTMainVC()
 
+@property (weak, nonatomic) IBOutlet UILabel *tempLabel;
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *humidityLabel;
+@property (weak, nonatomic) IBOutlet UILabel *pressureLabel;
+@property (weak, nonatomic) IBOutlet UILabel *speedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sunriseLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sunsetLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cityLabel;
 
 @end
 
@@ -43,18 +51,32 @@
            
            CurrentWeather* currentWeather = [[YTDBManager sharedManager] updateCurrentWeatherForToday:data];
            
-           NSLog(@"Weather data by ccord %@", data);
-           NSLog(@"Weather coreData object by ccord %@", currentWeather);
+           dispatch_async(dispatch_get_main_queue(), ^{
+               self.tempLabel.text = [NSString stringWithFormat:@"%.1f", [currentWeather.temp floatValue]];
+               self.descriptionLabel.text = [currentWeather.weatherDescription capitalizedString];
+               self.humidityLabel.text = [NSString stringWithFormat:@"Humidity: %li %%", [currentWeather.humidity integerValue]];
+               self.pressureLabel.text = [NSString stringWithFormat:@"Pressure: %.0f hPa", [currentWeather.pressure floatValue]];
+               self.speedLabel.text = [NSString stringWithFormat:@"Wind speed: %li m/s", [currentWeather.speed integerValue]];
+               NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+               [formatter setDateFormat:@"kk:mm"];
+               self.sunriseLabel.text = [NSString stringWithFormat:@"Sunrise: %@", [formatter stringFromDate:currentWeather.sunrise]];
+               self.sunsetLabel.text = [NSString stringWithFormat:@"Sunset: %@", [formatter stringFromDate:currentWeather.sunset]];
+           });
+           
+//           NSLog(@"Weather data by ccord %@", data);
+//           NSLog(@"Weather coreData object by ccord %@", currentWeather);
        }
        onFailure:^(NSError *error, NSInteger statusCode) {
            NSLog(@"%@", [error localizedDescription]);
     }];
     
     [[YTGoogleGeocodeManager sharedManager] getGeocodeInformationByCoordinates:coord onSuccess:^(NSString *info) {
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.cityLabel.text = [NSString stringWithFormat:@"%@", info];
+        });
         NSLog(@"current city is %@", info);
-    } onFailure:^(NSError *error, NSInteger statusCode) {
-        NSLog(@"%@", [error localizedDescription]);
-    }];
+    } onFailure:nil];
     
 /*
      convert timestamp to nsdate and formatter
